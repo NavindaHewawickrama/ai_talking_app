f6e379beab077839c79d815e79ecb5d2468e72d1
NavindaHewawickrama <hewawickraman@gmail.com>
2025-02-09 00:06:26 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/ai_app_1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ai_app_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
+import 'package:flutter_tts/flutter_tts.dart';
+import 'package:http/http.dart' as http;
+
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen>{
+  @override
+  _ChatScreenState createState() => _ChatScreenState(); 
+}
